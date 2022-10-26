9bf00faef7775a319b89a62963291f4221e33372
luiseduardomattos <114542913+luiseduardomattos@users.noreply.github.com>
2022-10-25 22:31:17 -0300

    vetor de filmes
    vetor de filmes

---
+++ app.js
@@ -0,0 +1,33 @@
+let filme01 = 'imagens/filme01.jpg'
+let filme02 = 'imagens/filme02.jpg'
+let filme03 = 'imagens/filme03.jpg'
+
+let filme04 = 'imagens/filme04.jpg'
+let filme05 = 'imagens/filme05.jpg'
+let filme06 = 'imagens/filme06.jpg'
+
+document.querySelector(".container").innerHTML += "<img src='" + filme01 + "'> "
+document.querySelector(".container").innerHTML += "<img src='" + filme02 + "'> "
+document.querySelector(".container").innerHTML += "<img src='" + filme03 + "'> "
+
+document.querySelector(".container").innerHTML += "<img src='" + filme04 + "'> "
+document.querySelector(".container").innerHTML += "<img src='" + filme05 + "'> "
+document.querySelector(".container").innerHTML += "<img src='" + filme06 + "'> "
+
+var filmes ['imagens/filme01.jpg', 'imagens/filme02.jpg', 'imagens/filme03.jpg','imagens/filme04.jpg', 'imagens/filme05.jpg', 'imagens/filme06.jpg']
+
+document.querySelector(".container").innerHTML += "<img src='" + filme01 + "'> "
+document.querySelector(".container").innerHTML += "<img src='" + filme02 + "'> "
+document.querySelector(".container").innerHTML += "<img src='" + filme03 + "'> "
+
+document.querySelector(".container").innerHTML += "<img src='" + filme04 + "'> "
+document.querySelector(".container").innerHTML += "<img src='" + filme05 + "'> "
+document.querySelector(".container").innerHTML += "<img src='" + filme06 + "'> "
+
+
+
+
+
+// 1 - acrescentar mais 3 filmes.
+// 2 - personalizar o layout (trocar filmes, fundo, cores, fontes).
+// 3 - armazenar os filmes em um único vetor (array).
